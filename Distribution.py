5deacfd965e0fbb516b413ae45aa6cb681aac111
Lisa <lisa.duheron@dauphine.eu>
2024-11-10 13:22:47 +0100

    Part 1: retrieve data, inelegant render

--- Distribution.py
+++ Distribution.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import matplotlib.pyplot as plt 
-import numpy as np
+
+# https://java2blog.com/python-count-files-directory/
 
 #  Pseudo code
 # - check given arg
@@ -11,7 +12,26 @@ import numpy as np
 #-  seperate function to pie
 # - show
 
-def main(data_directory):
+def pie(size, label):
+	fig, ax = plt.subplots()
+	ax.pie(size, labels=label, autopct='%1.1f%%')
+
+
+def main(path):
+	dir_label = []
+	count_file = []
+
+	for root, dirs, files in os.walk(path):
+		for dir in dirs:
+			dir_label.append(dir)
+			dir_path = os.path.join(root, dir)
+			count_file.append(len(os.listdir(dir_path)))
+
+	# print(count_file)
+	# print(dir_label)
+	pie(count_file, dir_label)
+	plt.bar(dir_label, count_file)
+	plt.show()
 
 
 
@@ -19,3 +39,4 @@ if __name__ == "__main__":
 	if len(sys.argv) != 2:
 		exit("Usage: ./Distribution.py directory")
 	main(sys.argv[1])
+
