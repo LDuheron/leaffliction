58a8b18f93ea722d2377d5d960774839975df663
Lisa <lisa.duheron@dauphine.eu>
2024-11-10 17:29:38 +0100

    Part2: Implement dictionary in augmentation(), program can rotate one image

--- Augmentation.py
+++ Augmentation.py
@@ -6,15 +6,37 @@ import cv2
 
 # Pseudo code
 # check given arg + join
-# apply filter on image
-# add extension name
-# save image
+# in main :
+# - loop over all images and aply ft augmentation
+# in augmentation :
+# - create a dictionary to store all modification
+# - apply all modifs on image
+# - add extension name
+# - save image with extension 
+
+# modification = { "filter1": "image with filter1", "filter2": "image with filter2"}
+#
+
+def augmentation(img, path):
+	modification = {}
+
+	modification["_Rotate.JPG"] = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+	# save image 
+	for key, value in modification.items():
+		save_path = path.split(".JPG")[0] + key
+		cv2.imwrite(save_path, value)
+
 
 def main(path):
 	join_path = os.path.join('.', path)
-	img = cv2.imread(join_path)
-	cv2.imshow('Leaf', img)
-	cv2.waitKey(0) # open cv will show the image until I press a key, ssinon si je mets 5000 ca rest 5 secondes
+	try:
+		img = cv2.imread(join_path)
+		augmentation(img, path)
+	except:
+		print("Error")
+
+
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
