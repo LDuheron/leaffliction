b2fad1ee05a911c85ab28302ee78e2eae7aaed12
Lisa <lisa.duheron@dauphine.eu>
2024-11-10 16:49:20 +0100

    Part2: cv2 test fix

--- Augmentation.py
+++ Augmentation.py
@@ -11,8 +11,9 @@ import cv2
 # save image
 
 def main(path):
-	img = cv2.imread(path)
-	cv2.imshow('Name', img)
+	join_path = os.path.join('.', path)
+	img = cv2.imread(join_path)
+	cv2.imshow('Leaf', img)
 	cv2.waitKey(0) # open cv will show the image until I press a key, ssinon si je mets 5000 ca rest 5 secondes
 
 if __name__ == "__main__":
