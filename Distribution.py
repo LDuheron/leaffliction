a232ee660f713c71abe494b808eda6dda5a7db75
Lisa <lisa.duheron@dauphine.eu>
2024-11-10 16:09:38 +0100

    Part1: Add colors with seaborn + filter directories without files + separate data per type of fruit

--- Distribution.py
+++ Distribution.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # https://java2blog.com/python-count-files-directory/
 
@@ -8,32 +9,46 @@ import matplotlib.pyplot as plt
 # - check given arg
 # - retrieve data from directory
 # - store data
-# - seperate function to plot
-#-  seperate function to pie
+# - separate fruits
+# - sort per fruit and per keys in list !
+# - seperate function pie and bar
 # - show
 
-def render(size, label):
+def render(fruit_type, label, size):
 	fig, (ax1, ax2) = plt.subplots(1, 2)
-	fig.suptitle("Number of images per directory")
+	fig.suptitle(fruit_type + " class distribution")
+	palette = sns.color_palette("Set3")
 
-	ax1.pie(size, labels=label, autopct='%1.1f%%')
+	ax1.pie(size, labels=label, colors= palette, autopct='%1.1f%%')
 
-	ax2.bar(label, size)
+	plt.xticks(rotation=60)
+	ax2.bar(label, size, color=palette)
+
+	plt.tight_layout()
 
 	plt.show()
 
+def get_label():
+	return
 
 def main(path):
-	dir_label = []
-	count_file = []
+	fruit_dictionary = {}
 
 	for root, dirs, files in os.walk(path):
 		for dir in dirs:
-			dir_label.append(dir)
-			dir_path = os.path.join(root, dir)
-			count_file.append(len(os.listdir(dir_path)))
+			if "_" in dir:
+				dir_path = os.path.join(root, dir)
+				files_in_dir = [item for item in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, item))]
+			
+				if files_in_dir:
+					fruit = dir.split("_")[0]
+					if fruit not in fruit_dictionary:
+						fruit_dictionary[fruit] = {"label": [], "size": []}
+					fruit_dictionary[fruit]["label"].append(dir)
+					fruit_dictionary[fruit]["size"].append(len(files_in_dir))
 
-	render(count_file, dir_label)
+	for fruit in sorted(fruit_dictionary):
+		render(fruit, fruit_dictionary[fruit]["label"], fruit_dictionary[fruit]["size"])
 
 
 if __name__ == "__main__":
